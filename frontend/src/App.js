f2deefee6a7196b226ed01c8810a2db0e8be1706
Seundede <omadoyedede0@gmail.com>
2022-11-17 20:23:44 +0100

    add footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,22 @@ import React from "react";
 import NavigationBar from "./components/NavigationBar";
 
 function App() {
+  const planData = [
+  {title: 'free'},
+  
+  ]
   return (
     <>
       <NavigationBar />
-      <div>
-        
+      <div className="bg-[#F5F5F5] w-full px-[178px] items-center leading-[100px] top-[193px]">
+        <h1 className="text-[#000000] text-[72px] font-semibold text-center">
+          An easy <span className="text-[#1570EF]">Sales and Accounting</span>
+          system made for you
+        </h1>
+        <p className="text-[#101828] font-normal text-[20px] text-center leading-[35px]">
+          Run your business effectively with a simplified solution to reconcile
+          your accounts & sales on a day - to - day basis
+        </p>
       </div>
     </>
   );
