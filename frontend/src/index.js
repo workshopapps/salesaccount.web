16e8d92e2641a44d4c6c35654d1cf43eac78a726
Seundede <omadoyedede0@gmail.com>
2022-11-18 14:52:35 +0100

    eslint, prettier and assets folder feature

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 
 const router = createBrowserRouter([
   {
