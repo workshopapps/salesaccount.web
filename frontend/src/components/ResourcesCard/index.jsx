88d165cfd93c69a14f577152567f79c4071eb9bc
Seun Olafisoye <86838623+olafisoyevince@users.noreply.github.com>
2022-11-19 13:29:11 +0100

    feat(TEA-107>-resources-page

--- frontend/src/components/ResourcesCard/index.jsx
+++ frontend/src/components/ResourcesCard/index.jsx
@@ -1,40 +1,53 @@
 import React from 'react';
-import cardimg from '../../assets/card-img.png';
 import arrowright from '../../assets/arrowright.svg';
+import Blogs from '../../assets/Blogs.png';
+import Help from '../../assets/Help.png';
+import Privacy from '../../assets/Privacy.png';
+import FAQ from '../../assets/FAQ.png';
+import Contact from '../../assets/Contact.png';
 
 function ResourcesCard() {
 	return (
-		<div className="flex flex-wrap justify-between">
-			<div className=" w-36 pt-5">
-				<img src={cardimg} alt="" />
-				<p className=" text-xs text-blue-500 pt-2">On demand data sheet</p>
+		<div className="flex py-5 flex-wrap gap-4 justify-center px:10 md:px-11 lg:px-6 xl:px-36 ">
+			<div className=" w-72 p-5 rounded-md drop-shadow border ">
+				<img src={Help} alt="" />
+				<p className=" text-md text-blue-500 pt-2">On demand data sheet</p>
 				<h1 className=" font-semibold py-3">
 					How to use the Account Pal For Multiple SME’s{' '}
 				</h1>
 				<img src={arrowright} alt="" />
 			</div>
 
-			<div className=" w-36 pt-5">
-				<img src={cardimg} alt="" />
-				<p className=" text-xs text-blue-500 pt-2">On demand data sheet</p>
+			<div className=" w-72 p-5 rounded-md drop-shadow border">
+				<img src={Privacy} alt="" />
+				<p className=" text-md text-blue-500 pt-2">On demand data sheet</p>
 				<h1 className=" font-semibold py-3">
 					How to use the Account Pal For Multiple SME’s{' '}
 				</h1>
 				<img src={arrowright} alt="" />
 			</div>
 
-			<div className=" w-36 pt-5">
-				<img src={cardimg} alt="" />
-				<p className=" text-xs text-blue-500 pt-2">On demand data sheet</p>
+			<div className=" w-72 p-5 rounded-md drop-shadow border">
+				<img src={Blogs} alt="" />
+				<p className=" text-md text-blue-500 pt-2">On demand data sheet</p>
 				<h1 className=" font-semibold py-3">
 					How to use the Account Pal For Multiple SME’s{' '}
 				</h1>
 				<img src={arrowright} alt="" />
 			</div>
 
-			<div className=" w-36 pt-5">
-				<img src={cardimg} alt="" />
-				<p className=" text-xs text-blue-500 pt-2">On demand data sheet</p>
+			<div className=" w-72 p-5 rounded-md drop-shadow border">
+				<img src={FAQ} alt="" />
+				<p className=" text-md text-blue-500 pt-2">On demand data sheet</p>
+				<h1 className=" font-semibold py-3">
+					How to use the Account Pal For Multiple SME’s{' '}
+				</h1>
+				<img src={arrowright} alt="" />
+			</div>
+
+			<div className=" w-72 p-5 rounded-md drop-shadow border">
+				<img src={Contact} alt="" />
+				<p className=" text-md text-blue-500 pt-2">On demand data sheet</p>
 				<h1 className=" font-semibold py-3">
 					How to use the Account Pal For Multiple SME’s{' '}
 				</h1>
--- frontend/src/components/ResourcesHeader/index.jsx
+++ frontend/src/components/ResourcesHeader/index.jsx
@@ -1,42 +1,25 @@
 import React from 'react';
-import resourcesheaderimg from '../../assets/resources-header-img.png';
+import resourcesheaderimg from '../../assets/Capa 1.png';
 // import hexagonfill from '../../assets/hexagon-fill.svg';
 // import hexagonoutline from '../../assets/hexagon-outline.svg';
 
 function ResourcesHeader() {
 	return (
-		<div className=" px-4 bg-blue-300">
-			<div className="  py-5 flex justify-center items-center flex-col ">
-				<h1 className=" text-2xl font-semibold pb-4">Videos</h1>
+		<div className=" px-4 bg-blue-200">
+			<div className="  pt-5 flex justify-center items-center flex-col ">
+				<h1 className=" text-2xl md:text-4xl font-semibold pt-9">Resources</h1>
 
-				<div className="flex items-center gap-4">
-					<img className=" max-h-20" src={resourcesheaderimg} alt="" />
+				<p className=" text-sm text-center px-5 py-7">
+					Get everything done with all our resources. Click on the resources
+					you’d like to explore.
+				</p>
 
-					<div>
-						<p className=" text-blue-500">Account PAL Videos</p>
-						<h1 className=" font-semibold py-2">Virtual Hands-on Lab</h1>
-						<button
-							type="button"
-							className=" text-white bg-blue-500 px-4 py-2 rounded-lg"
-						>
-							Watch Now
-						</button>
-					</div>
-				</div>
-
-				<div className="relative">
-					{/* <div>
-						<img className="absolute" src={hexagonfill} alt="" />
-						<img
-							className="absolute bottom-0 left-0 z-50"
-							src={hexagonoutline}
-							alt=""
-						/>
-					</div> */}
-					<p className=" text-xs text-center px-5 py-4">
-						Find out about all our events below. Click on the resources you’d
-						like to explore.
-					</p>
+				<div className=" text-center">
+					<img
+						className=" mx-auto max-h-20 md:max-h-32"
+						src={resourcesheaderimg}
+						alt=""
+					/>
 				</div>
 			</div>
 		</div>
