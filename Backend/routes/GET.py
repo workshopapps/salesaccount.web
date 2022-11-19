56c28f1ac03059a68e019aa84af87a66e7e3c681
Oluwabunmi Fife <oluwabunmifife@gmail.com>
2022-11-19 11:23:40 +0100

    chore(TEA-61): removed origins list and made correction to CORSMiddleware

--- Backend/routes/GET.py
+++ Backend/routes/GET.py
@@ -4,14 +4,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "localhost:3000"
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
