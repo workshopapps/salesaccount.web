0e11181b7cc6cd4b79b3a04431ec24d871eb8481
Seundede <omadoyedede0@gmail.com>
2022-11-19 18:05:35 +0100

    styling added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,9 @@ import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
 import company_logo from './assets/company_images.png'
 import header_image from './assets/header_image.png'
 import cutting_image from './assets/cutting edge.png'
+import mobile_dashboard from './assets/mobile_dashboard.png'
+import mobile_company from './assets/mobile_company.png'
+import iphone_screen from './assets/iPhone 12 Pro mockup.png'
 
 const data = {
   title: "Frequently asked questions ",
@@ -170,52 +173,66 @@ const trustedByFeaturesData = [
 function App() {
   return (
     <div>
-      <NavigationBar />
-      <div className="bg-[#EFF8FF] flex flex-col items-center justify-center:gap-y-[8px] md:gap-y-[16px] z-40 relative w-full">
-        <div className=" w-[273px] md:w-[1085px] leading-[92px] pt-[50px] ">
+      {/**<NavigationBar /> */}
+      <div className="bg-[#EFF8FF] flex flex-col items-center justify-center gap-y-[8px] md:gap-y-[16px] z-40 relative w-full">
+        <div className=" w-[273px] lg:w-[1085px] lg:leading-[92px] pt-[50px] ">
           <h1 className="text-[#101828] text-[28px] md:text-[72px] font-semibold text-center">
             An easy <span className="text-[#1570EF]">Sales and Accounting</span>{" "}
             system made for you
           </h1>
         </div>
-        <div className=" w-[850px] pb-[24px]">
-          <p className="text-[#101828] font-normal text-[20px] text-center">
+        <div className="w-[273px] lg:w-[850px] pb-[24px]">
+          <p className="text-[#101828] font-normal text-[12px] lg:text-[20px] text-center">
             Run your business effectively with a simplified solution to
             reconcile your accounts & sales on a day - to - day basis
           </p>
         </div>
 
         <Button title="Dashboard" path="/" />
-        <img src={header_image} alt="Dashboard" className="w-[500px]" />
+        <img
+          src={header_image}
+          alt="Dashboard"
+          className=" hidden md:flex w-[500px]"
+        />
+        <img src={mobile_dashboard} alt="Dashboard" className=" md:hidden" />
       </div>
       {/**Trusted by */}
-      <div className="bg-[#F9FAFB] flex flex-col  gap-y-[30px] mt-[-160px] z-50 relative">
-        <h2 className="pt-[25px] font-semibold text-[20px] text-[#344054] text-center">
+      <div className="bg-[#F9FAFB] flex flex-col gap-y-[24px]  md:gap-y-[30px] md:mt-[-160px] md:z-50 md:relative">
+        <h2 className="md:pt-[25px] pt-[20px] font-semibold md:text-[20px] text-[12px] text-[#344054] text-center">
           Trusted By 100,000+ companies in 179 countries
         </h2>
-        <div className="w-full px-[110px] pt-[26px] pb-[66px]">
-          <img src={company_logo} alt="company" className="w-screen" />
+        <div className="w-full px-[44px] md:px-[110px] md:pt-[26px] pt-[24px] pb-[40px] md:pb-[66px]">
+          <img
+            src={company_logo}
+            alt="company"
+            className="w-screen hidden md:flex"
+          />
+          <img
+            src={mobile_company}
+            alt="company"
+            className="w-screen md:hidden"
+          />
         </div>
 
-        <div className="flex flex-col gap-y-[1px] items-center">
-          <h1 className="font-semibold text-[#101828] text-[36px]">
+        <div className="flex flex-col gap-y-[1px] items-center px-[44px] md:px-0">
+          <h1 className="font-semibold text-[#101828] md:text-[36px] text-[20px] text-center">
             Balance your account in 3 easy steps
           </h1>
-          <p className="text-[#667085] text-[20px] font-normal">
+          <p className="text-[#667085] md:text-[20px] text-[16px] font-normal">
             Balance your account in 3 Steps
           </p>
         </div>
-        <div className="flex flex-row gap-[120px] px-[110px] pb-[68px]">
+        <div className="flex flex-col md:flex-row gap-[10px] md:gap-[120px] px-[44px] md:px-[110px] pb-[34px] md:pb-[68px]">
           {trustedByFeaturesData.map((item) => (
             <div className="flex flex-col items-center w-full ">
-              <h3 className="text-[#101828] font-bold text-[80px]">
+              <h3 className="text-[#101828] font-bold text-[56px] md:text-[80px]">
                 {item.number}
               </h3>
-              <h3 className="text-[#101828] font-semibold text-center text-[20px]">
+              <h3 className="text-[#101828] font-semibold text-center text-[16px] md:text-[20px]">
                 {item.title}
               </h3>
               <div className="w-[300px] text-center">
-                <p className="text-[#667085] font-normal text-[16px] ">
+                <p className="text-[#667085] font-normal text-[14px] md:text-[16px] ">
                   {item.content}
                 </p>
               </div>
@@ -226,21 +243,21 @@ function App() {
       {/**Pick your plan */}
       <div className="bg-[#F9FAFB]">
         <div className="pt-[48px] text-center">
-          <h1 className="text-[#101828] font-semibold text-[36px]">
+          <h1 className="text-[#101828] font-semibold text-[28px] md:text-[36px]">
             Pick Your Plan
           </h1>
-          <p className="text-[#667085]">
+          <p className="text-[#667085] text-[16px]">
             Choose a plan that suits your Business
           </p>
         </div>
-        <div className="flex flex-row gap-[64px] pt-[60px] items-center justify-center px-[110px] pb-[92px]">
+        <div className="flex flex-col md:flex-row gap-[64px] pt-[60px] items-center px-[44px] lg:px-[110px] pb-[57px] lg:pb-[92px]">
           {pickPlanData.map((item) => (
-            <div className="flex flex-col border border-[#D0D5DD] text-center w-full ">
-              <div className="h-[444px] w-[300px] flex flex-col items-center">
-                <h2 className=" text-[#101828] font-semibold pt-[41px] text-[24px]">
+            <div className="flex flex-col border border-[#D0D5DD] items-center text-center w-full ">
+              <div className=" h-[330px] lg:h-[444px] w-[300px] flex flex-col items-center">
+                <h2 className=" text-[#101828] font-semibold md:pt-[41px] pt-[30px] text-[21px] md:text-[24px]">
                   {item.title}
                 </h2>
-                <div className=" font-normal text-[#858585] flex flex-col items-center gap-y-[20px] pt-[30px]">
+                <div className=" font-normal text-[#858585] flex flex-col items-center gap-y-[20px] md:pt-[30px]">
                   <div className="flex items-center ">
                     <p className="pr-[8px]">{item.icon1}</p>
                     <p className="text-[16px]">{item.content1}</p>
@@ -257,7 +274,7 @@ function App() {
                     <p className="pr-[8px] ">{item.icon4}</p>
                     <p className="text-[16px]">{item.content4}</p>
                   </div>
-                  <div className="cursor-pointer text-[#101828] font-semibold text-[14px] border border-[#858585] px-[28px] py-[16px] rounded-md mt-[18px]">
+                  <div className="cursor-pointer text-[#101828] font-semibold text-[14px] border border-[#858585] px-[28px] py-[16px] rounded-md mt-[8px] md:mt-[18px]">
                     {item.button_content}
                   </div>
                 </div>
@@ -268,30 +285,38 @@ function App() {
       </div>
 
       {/**Cutting edge feature */}
-      <div className="bg-[#EFF8FF] flex flex-col items-center">
-        <h2 className="text-[#101828] pt-[108px] font-semibold text-[36px]">
+      <div className="bg-[#EFF8FF] flex flex-col items-center px-[44px] md:px-0">
+        <h2 className="text-[#101828] md:pt-[108px] text-center font-medium md:font-semibold text-[24px] md:text-[36px]">
           Cutting-edge features for advanced analytics
         </h2>
-        <div className="w-[700px] pb-[64px]">
-          <p className="font-normal text-[#667085] text-center text-[20px] pt-[8px]">
+        <div className="w-[289px] lg:w-[700px] pb-[64px]">
+          <p className="font-normal text-[#667085] text-center text-[14px] md:text-[20px] pt-[8px]">
             Powerful, self-serve product and growth analytics to help you
             convert, match sales. Trusted by over 4,000 startups.
           </p>
         </div>
-        <div></div>
-        <img src={cutting_image} alt="Dashboard" className="pb-[48px]" />
+        <img
+          src={cutting_image}
+          alt="Dashboard"
+          className="hidden md:flex pb-[48px]"
+        />
+        <img
+          src={iphone_screen}
+          alt="Dashboard"
+          className="md:hidden pb-[48px]"
+        />
       </div>
       {/**What account pal does for you */}
-      <div className="bg-[#F9FAFB]">
-        <h1 className="text-[#101828] font-semibold text-center text-[36px] pt-[48px]">
+      <div className=" bg-[#F9FAFB] relative mt-[-200px] md:mt-0">
+        <h1 className="text-[#101828] font-semibold text-center text-[28px] md:text-[36px] pt-[24px] md:pt-[48px] px-[44px] md:px-0">
           What Account Pal Does for You
         </h1>
-        <p className="text-[#667085] font-normal text-center text-[20px] pt-[8px] pb-[24px]">
+        <p className="text-[#667085] font-normal text-center text-[12px] md:text-[20px] pt-0 md:pt-[8px] pb-[24px]">
           Important Features
         </p>
-        <div className="flex flex-row items-center gap-[100px] px-[110px]">
+        <div className="flex flex-col md:flex-row items-center md:gap-[100px] px-[44px] md:px-[110px]">
           {whatAccountPalDoesData.map((item) => (
-            <div className="w-[400px] ">
+            <div className="md:w-[400px] ">
               <h2 className="font-semibold text-[#101828] text-[16px] pb-[4px] text-center">
                 {item.title}
               </h2>
@@ -303,7 +328,7 @@ function App() {
         </div>
       </div>
       {/**Important features */}
-      <div className="bg-[#F2F4F7]">
+      <div className="bg-[#F2F4F7] hidden md:flex">
         <h1 className="text-[#101828] font-semibold text-center text-[36px] pt-[48px]">
           Important Features
         </h1>
@@ -324,10 +349,10 @@ function App() {
 
       {/**What our users are saying */}
       <div className="bg-[#F9FAFB] flex flex-col items-center">
-        <h1 className="text-[#101828] font-semibold text-[36px] text-center pb-[32px] pt-[48px]">
+        <h1 className="text-[#101828] font-semibold text-[28px] md:text-[36px] text-center pb-[24px] md:pb-[32px] pt-[48px]">
           What our users are saying
         </h1>
-        <div className="w-[540px] h-[206px] rounded-2xl border border-[#98A2B3] flex flex-col items-center gap-[6px]">
+        <div className="md:w-[540px] md:h-[206px] rounded-2xl border border-[#98A2B3] flex flex-col items-center gap-[6px]">
           <p className="text-[#667085] text-[16px] font-medium px-[8px] pt-[18px] text-center">
             As an account that struggles to balance account statement
             accurately, account Pal has made life better for me. Now I can sum
--- frontend/src/components/NavigationBar/index.js
+++ frontend/src/components/NavigationBar/index.js
@@ -1,12 +1,10 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { BsSearch } from "react-icons/bs";
 import { BsGlobe2 } from "react-icons/bs";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import Button from "../Button";
 import { Link } from "react-router-dom";
 import logo from "../../assets/Logo Main Color.png";
-import { FcMenu } from 'react-icons/fc'
-import { AiOutlineClose } from 'react-icons/ai';
 
 const index = () => {
     const navLinks = [
@@ -15,23 +13,17 @@ const index = () => {
       { name: "Resources", path: "/resources" },
       { name: "Company", path: "/company" },
     ];
-     // eslint-disable-next-line react-hooks/rules-of-hooks
-     const [nav, setNav] = useState(false);
-
-     const toggle = () => {
-       setNav(!nav);
-     };
   return (
     <nav className="w-full h-[96px] px-[72px] flex items-center justify-between">
       <img src={logo} alt="Company logo" className="cursor-pointer" />
-      <div className="text-[20px] hidden lg:flex">
+      <div className="text-[20px]">
         {navLinks.map((item) => (
           <Link to={item.path} className="px-[30px]">
             {item.name}
           </Link>
         ))}
       </div>
-      <div className="hidden lg:flex items-center space-x-4 cursor-pointer">
+      <div className="flex items-center space-x-4 cursor-pointer">
         <BsSearch size={24} />
 
         <div className="flex items-center">
@@ -40,37 +32,6 @@ const index = () => {
         </div>
         <Button title="Sign up" path="/signup" primary />
         <Button title="Sign in" path="/signin" />
-      </div>
-
-      {/* Hamburger */}
-      <div className=" block lg:hidden" onClick={toggle}>
-        {nav ? <AiOutlineClose size={28} /> : <FcMenu size={28} />}
-      </div>
-      {/* mobile menu */}
-      <div
-        className={
-          nav
-            ? "lg:hidden w-full absolute top-[96px] left-0 p-2 flex justify-center relative  z-40  h-screen bg-gray-100 "
-            : "absolute left-[-100%]"
-        }
-      >
-        <div className="text-[20px] hidden lg:flex">
-          {navLinks.map((item) => (
-            <Link to={item.path} className="px-[30px]">
-              {item.name}
-            </Link>
-          ))}
-        </div>
-        <div className="hidden lg:flex items-center space-x-4 cursor-pointer">
-          <BsSearch size={24} />
-
-          <div className="flex items-center">
-            <BsGlobe2 size={24} />
-            <MdOutlineKeyboardArrowDown size={24} />
-          </div>
-          <Button title="Sign up" path="/signup" primary />
-          <Button title="Sign in" path="/signin" />
-        </div>
       </div>
     </nav>
   );
