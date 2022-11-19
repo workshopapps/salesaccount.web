1a6afc27bf739e6dd222ecdc988ddc33b031e386
Seundede <omadoyedede0@gmail.com>
2022-11-19 23:15:26 +0100

    add button.jsx

--- frontend/src/components/Button/index.js
+++ frontend/src/components/Button/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({ title, path, primary }) => 
+	(
+		<Link
+			to={path}
+			className={`rounded-md py-[10px] px-[18px] cursor-pointer text-[16px] ${
+				primary === true
+					? 'bg-[#FFFFFF}] text-[#1570EF] border border-[#1570EF]'
+					: 'bg-[#2E90FA] text-white'
+			}  `}
+		>
+			{title}
+		</Link>
+	);
+
 
 export default Button
