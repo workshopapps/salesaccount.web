d9f7f3ecd7af7b5c97e640fe9918f3f6b2055b25
skillzo <skillzfire@yahoo.com>
2022-11-20 13:27:14 +0100

    add doc page

---
+++ frontend/src/pages/Ex-Guide/ExGuide.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './Pages/Main';
+
+function ExGuide() {
+	return (
+		<div>
+			<Main />
+		</div>
+	);
+}
+
+export default ExGuide;
--- frontend/src/pages/Ex-Guide/Pages/Main.jsx
+++ frontend/src/pages/Ex-Guide/Pages/Main.jsx
@@ -1,23 +1,25 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import SideBar from "../Components/Navbar/SideBar";
-import SubNav from "../Components/SubNav/SubNav";
-import Wrapper from "../Components/UI/Wrapper/Wrapper";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import SideBar from '../Components/Navbar/SideBar';
+import SubNav from '../Components/SubNav/SubNav';
+import Wrapper from '../Components/UI/Wrapper/Wrapper';
+import Section21 from '../Components/Section/Section2/Section2Outlets/section21/Section21';
 
 function Main() {
-  return (
-    <>
-      <SubNav />
-      <div className="flex">
-        <SideBar />
-        <Wrapper>
-          <div className="md:w-[60%] md:ml-[4em]">
-            <Outlet />
-          </div>
-        </Wrapper>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<SubNav />
+			<div className="flex">
+				<SideBar />
+				<Wrapper>
+					<div className="md:w-[60%] md:ml-[4em]">
+						<Section21 />
+						{/* <Outlet /> */}
+					</div>
+				</Wrapper>
+			</div>
+		</>
+	);
 }
 
 export default Main;
