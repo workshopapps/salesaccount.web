e15093e2eceae1ca7bd2aff3760fc5baa538ba49
caleb-ali <slimkally@gmail.com>
2022-11-20 14:55:52 +0100

    adjusted margin and made corrections to syntax

--- frontend/src/pages/Teams/index.jsx
+++ frontend/src/pages/Teams/index.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable */
+
 
 import React from 'react';
 import Navbar from "../../components/NavigationBar";
@@ -20,139 +20,139 @@ function Teams() {
 	return (
     <div>
       <Navbar/>
-		<div class="m-8">
-			<div class="lg:flex lg:flex-row md:flex md:flex-row  flex-col  grid justify-items-center">
-				<div class="font-lexend lg:mt-24 md:mt-24">
-					<button class="rounded-lg lg:ml-2 ml-16 md:ml-2 text-white bg-blue-500 text-lg px-4 py-2">
+		<div className="px-[44px] xl:px-[72px] mt-8">
+			<div className="lg:flex lg:flex-row md:flex md:flex-row  flex-col  grid justify-items-center">
+				<div className="font-lexend lg:mt-24 md:mt-24">
+					<button type='button' className="rounded-lg lg:ml-2 ml-16 md:ml-2 text-white bg-blue-500 text-lg px-4 py-2">
 						Meet the team
 					</button>
-					<h2 class="lg:text-5xl text-2xl font-semibold mt-4 lg:text-left md:text-left text-center">
-						Meet our leadership <br class="lg:hidden" /> team, great{' '}
-						<br class="" /> personalities who work
-						<br class="lg:hidden" />
+					<h2 className="lg:text-5xl text-2xl font-semibold mt-4 lg:text-left md:text-left text-center">
+						Meet our leadership <br className="lg:hidden" /> team, great{' '}
+						<br className="" /> personalities who work
+						<br className="lg:hidden" />
 						hard to
-						<br class="hidden lg:block" /> empower our <br class="lg:hidden" />
+						<br className="hidden lg:block" /> empower our <br className="lg:hidden" />
 						teams.
 					</h2>
 				</div>
 				<div>
-					<img src={ourteam} class="ml-24 hidden lg:block md:block md:ml-8" />
+					<img alt='' src={ourteam} className="ml-24 hidden lg:block md:block md:ml-8" />
 				</div>
 			</div>
 
-			<div class="mt-12 flex flex-col items-center">
-				<div class="flex flex-row lg:gap-36 gap-2">
-					<div class=" flex flex-col items-center">
-						<img src={one} class="w-40 h-40 " />
-						<h3 class="font-medium">Oluwasegun Akinshola</h3>
-						<h4 class="text-blue-400 font-normal text-sm">
+			<div className="mt-12 flex flex-col items-center">
+				<div className="flex flex-row lg:gap-36 gap-2">
+					<div className=" flex flex-col items-center">
+						<img alt='' src={one} className="w-40 h-40 " />
+						<h3 className="font-medium">Oluwasegun Akinshola</h3>
+						<h4 className="text-blue-400 font-normal text-sm">
 							cheif Drill-Master
 						</h4>
-						<div class="mt-4 flex flex-row gap-6">
-							<img src={tw} />
-							<img src={ln} />
-							<img src={fb} />
+						<div className="mt-4 flex flex-row gap-6">
+							<img alt='' src={tw} />
+							<img alt='' src={ln} />
+							<img alt='' src={fb} />
 						</div>
 					</div>
 
-					<div class="flex flex-col items-center">
-						<img src={two} class="w-40 h-40" />
-						<h3 class="font-medium">Dorothy Nkatha</h3>
-						<h4 class="text-blue-400 font-normal text-sm">
+					<div className="flex flex-col items-center">
+						<img alt='' src={two} className="w-40 h-40" />
+						<h3 className="font-medium">Dorothy Nkatha</h3>
+						<h4 className="text-blue-400 font-normal text-sm">
 							Assist Drill-Master
 						</h4>
-						<div class="mt-4 flex flex-row gap-6">
-							<img src={tw} />
-							<img src={ln} />
-							<img src={fb} />
+						<div className="mt-4 flex flex-row gap-6">
+            <img alt='' src={tw} />
+							<img alt='' src={ln} />
+							<img alt='' src={fb} />
 						</div>
 					</div>
 				</div>
 
-				<div class="lg:flex lg:flex-row grid grid-cols-2 lg:gap-36 mt-12 gap-2">
-					<div class="flex flex-col items-center">
-						<img src={three} class="w-40 h-40" />
-						<h3 class="">Maureen Ehiosun</h3>
-						<h4 class="text-blue-400 font-normal text-sm">Design PM Lead</h4>
-						<div class="mt-4 flex flex-row gap-6">
-							<img src={tw} />
-							<img src={ln} />
-							<img src={fb} />
+				<div className="lg:flex lg:flex-row grid grid-cols-2 lg:gap-36 mt-12 gap-2">
+					<div className="flex flex-col items-center">
+						<img alt='' src={three} className="w-40 h-40" />
+						<h3 className="">Maureen Ehiosun</h3>
+						<h4 className="text-blue-400 font-normal text-sm">Design PM Lead</h4>
+						<div className="mt-4 flex flex-row gap-6">
+            <img alt='' src={tw} />
+							<img alt='' src={ln} />
+							<img alt='' src={fb} />
 						</div>
 					</div>
 
-					<div class="flex flex-col items-center">
-						<img src={four} class="w-40 h-40" />
-						<h3 class="">Olatunji David</h3>
-						<h4 class="text-blue-400 font-normal text-sm">Frontend PM Lead</h4>
-						<div class="mt-4 flex flex-row gap-6">
-							<img src={tw} />
-							<img src={ln} />
-							<img src={fb} />
+					<div className="flex flex-col items-center">
+						<img alt='' src={four} className="w-40 h-40" />
+						<h3 className="">Olatunji David</h3>
+						<h4 className="text-blue-400 font-normal text-sm">Frontend PM Lead</h4>
+						<div className="mt-4 flex flex-row gap-6">
+            <img alt='' src={tw} />
+							<img alt='' src={ln} />
+							<img alt='' src={fb} />
 						</div>
 					</div>
 
-					<div class="flex flex-col items-center lg:mt-0 mt-12">
-						<img src={five} class="w-40 h-40" />
-						<h3 class="">AbdulMuizz Ikumapayi</h3>
-						<h4 class="text-blue-400 font-normal text-sm">
+					<div className="flex flex-col items-center lg:mt-0 mt-12">
+						<img alt='' src={five} className="w-40 h-40" />
+						<h3 className="">AbdulMuizz Ikumapayi</h3>
+						<h4 className="text-blue-400 font-normal text-sm">
 							Backend Track Lead
 						</h4>
-						<div class="mt-4 flex flex-row gap-6">
-							<img src={tw} />
-							<img src={ln} />
-							<img src={fb} />
+						<div className="mt-4 flex flex-row gap-6">
+            <img alt='' src={tw} />
+							<img alt='' src={ln} />
+							<img alt='' src={fb} />
 						</div>
 					</div>
 
-					<div class="flex flex-col items-center lg:mt-0 mt-12">
-						<img src={two} class="w-40 h-40" />
-						<h3 class="">Amogbe Idowu Esther</h3>
-						<h4 class="text-blue-400 font-normal text-sm">
+					<div className="flex flex-col items-center lg:mt-0 mt-12">
+						<img alt='' src={two} className="w-40 h-40" />
+						<h3 className="">Amogbe Idowu Esther</h3>
+						<h4 className="text-blue-400 font-normal text-sm">
 							Sales and Marketing PM Lead
 						</h4>
-						<div class="mt-4 flex flex-row gap-6">
-							<img src={tw} />
-							<img src={ln} />
-							<img src={fb} />
+						<div className="mt-4 flex flex-row gap-6">
+            <img alt='' src={tw} />
+							<img alt='' src={ln} />
+							<img alt='' src={fb} />
 						</div>
 					</div>
 				</div>
-				<div class="lg:flex lg:flex-row grid grid-cols-2 lg:gap-36 mt-12 gap-2">
-					<div class="flex flex-col items-center">
-						<img src={seven} class="w-40 h-40" />
-						<h3 class="">Emmanuel Job</h3>
-						<h4 class="text-blue-400 font-normal text-sm">Design Track Lead</h4>
-						<div class="mt-4 flex flex-row gap-6">
-							<img src={tw} />
-							<img src={ln} />
-							<img src={fb} />
+				<div className="lg:flex lg:flex-row grid grid-cols-2 lg:gap-36 mt-12 gap-2">
+					<div className="flex flex-col items-center">
+						<img alt='' src={seven} className="w-40 h-40" />
+						<h3 className="">Emmanuel Job</h3>
+						<h4 className="text-blue-400 font-normal text-sm">Design Track Lead</h4>
+						<div className="mt-4 flex flex-row gap-6">
+            <img alt='' src={tw} />
+							<img alt='' src={ln} />
+							<img alt='' src={fb} />
 						</div>
 					</div>
 
-					<div class="flex flex-col items-center">
-						<img src={eight} class="w-40 h-40" />
-						<h3 class="">Queenette oluchi</h3>
-						<h4 class="text-blue-400 font-normal text-sm">
+					<div className="flex flex-col items-center">
+						<img alt='' src={eight} className="w-40 h-40" />
+						<h3 className="">Queenette oluchi</h3>
+						<h4 className="text-blue-400 font-normal text-sm">
 							Sales and Marketing Track Lead
 						</h4>
-						<div class="mt-4 flex flex-row gap-6">
-							<img src={tw} />
-							<img src={ln} />
-							<img src={fb} />
+						<div className="mt-4 flex flex-row gap-6">
+            <img alt='' src={tw} />
+							<img alt='' src={ln} />
+							<img alt='' src={fb} />
 						</div>
 					</div>
 
-					<div class="flex flex-col items-center">
-						<img src={nine} class="w-40 h-40" />
-						<h3 class="">Abdhakeem Abdurahmon</h3>
-						<h4 class="text-blue-400 font-normal text-sm">
+					<div className="flex flex-col items-center mb-8">
+						<img alt='' src={nine} className="w-40 h-40" />
+						<h3 className="">Abdhakeem Abdurahmon</h3>
+						<h4 className="text-blue-400 font-normal text-sm">
 							Frontend Track Lead
 						</h4>
-						<div class="mt-4 flex flex-row gap-6">
-							<img src={tw} />
-							<img src={ln} />
-							<img src={fb} />
+						<div className="mt-4 flex flex-row gap-6">
+            <img alt='' src={tw} />
+							<img alt='' src={ln} />
+							<img alt='' src={fb} />
 						</div>
 					</div>
 				</div>
