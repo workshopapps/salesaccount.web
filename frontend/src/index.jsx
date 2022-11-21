8a0bb95b210593e294f4bfb0910deba593d49e74
Seun Olafisoye <86838623+olafisoyevince@users.noreply.github.com>
2022-11-21 15:17:15 +0100

    fix: resolving issues

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -38,7 +38,7 @@ const router = createBrowserRouter([
 		element: <AboutUs />,
 	},
 	{
-		path: '/Dashboard',
+		path: '/dashboard',
 		element: <DashBoard />,
 	},
 	{
@@ -47,9 +47,9 @@ const router = createBrowserRouter([
 	},
 
 	{
-		path: "/blog/:slug",
-		element: <BlogPost/>
-	  },
+		path: '/blog/:slug',
+		element: <BlogPost />,
+	},
 
 	{
 		path: '/careers',
@@ -113,7 +113,7 @@ const router = createBrowserRouter([
 
 	{
 		path: 'userupload',
-		element: <Userupload/>,
+		element: <Userupload />,
 	},
 
 	{
@@ -135,9 +135,9 @@ const router = createBrowserRouter([
 		element: <Gpt3 />,
 	},
 	{
-		path: "/blogs",
-		element: <Blogs />
-	  },
+		path: '/blogs',
+		element: <Blogs />,
+	},
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
