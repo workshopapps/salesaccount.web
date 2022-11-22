d278f9650e2c90f239b7dfcdadc3d0969c8a181d
Adeolaking <adeolakinga@gmail.com>
2022-11-22 15:59:48 +0100

    changed the dashboard

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -4,13 +4,13 @@ import DashBoardSideBar from '../../components/DashBoardSideBar';
 import DashBoardHeader from '../../components/DashBoardHeader';
 
 const DashBoard = () => (
-	<>
+	<div className='w-100'>
 		<DashBoardHeader />
 		<div className="dashboard-main-cont">
 			<DashBoardSideBar />
 			<main>dashboard</main>
 		</div>
-	</>
+	</div>
 );
 
 export default DashBoard;
