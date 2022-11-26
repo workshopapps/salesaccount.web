67b9a6a2fedd323d2511fbc056178a93a0c60577
Naseerj <jenrolaolasupo@gmail.com>
2022-11-26 11:46:43 +0100

    Updated privacy

--- frontend/src/components/DashBoardSideBar/index.jsx
+++ frontend/src/components/DashBoardSideBar/index.jsx
@@ -35,11 +35,11 @@ function DashBoardSideBar() {
 			</div>
 
 			<div className="flex flex-col w-[95%] md:gap-6 lg:gap-6">
-				<div className="flex sidenav-nav space-x-1">
+				<Link to='/privacy'><div className="flex sidenav-nav space-x-1">
 					<PersonIcon sx={{ fontSize: 20 }} />
 					<p>Privacy</p>
 				</div>
-
+</Link>
 				<div className="flex sidenav-nav space-x-1">
 					<LightbulbOutlinedIcon sx={{ fontSize: 20 }} />
 					<p>Help</p>
