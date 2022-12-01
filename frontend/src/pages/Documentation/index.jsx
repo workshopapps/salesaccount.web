f13e4568c252f31e691487d6d4356f6aebe00e78
Adeolaking <adeolakinga@gmail.com>
2022-12-01 11:49:18 +0100

    included all text

--- frontend/src/pages/Documentation/index.jsx
+++ frontend/src/pages/Documentation/index.jsx
@@ -10,7 +10,7 @@ const Documentation = () => (
 		<header className="header bg-[#2E90FA] text-[#F9FAFB] shadow py-4 px-4">
 			<div className="header-content h-40 flex items-center justify-center flex-row">
 				<h1 className="text-3xl text-center">
-					A step by step guide on how <br /> to use Accountpal
+					A step by step guide on how <br /> to use Recon
 				</h1>
 			</div>
 		</header>
@@ -42,97 +42,109 @@ const Documentation = () => (
 				<div className="main-content flex flex-col flex-grow p-4">
 					<div className="flex flex-col flex-grow   rounded mt-4">
 						<section className="mb-11">
-							<h1 className="text-2xl mb-4">Introduction</h1>
-							<p>
+							<h1 className="text-2xl font-bold mb-4  ">Introduction</h1>
+              <div className=''> </div>
+							<p className='mb-5'>
 								Recon is a web application that compares third-party and
 								independent financial statements and records with internal
 								financial records and ledgers. It does this accurately with
 								artificial intelligence from Open AI and machine learning models
 								like GPT-3 paving the way for even smoother future matchings and
-								collaboration of accounts from vendors. Accountpal makes the
+								collaboration of accounts from vendors. Recon makes the
 								day-to-day balancing of accounts and sales easier, faster, and
 								very reliable for Small and Medium Enterprises.
 							</p>
-						</section>
-						<section className="mb-11">
-							<h1 className="text-2xl mb-4">Why Recon?</h1>
-							<p>
-								{' '}
-								A web application that makes the day-today balancing of accounts
-								and sales easier, faster and very much reliable is needed in all
-								industries. That is what makes Accountpal a go-to software fo
-								all your account balancing.
-							</p>
 
-							<p>
-								Who It&apos;s For Freelancers: Accurate billing and lots of
+              <h1 className="font-bold text-2xl mb-4">Who It&apos;s For?</h1>
+							
+
+							<p className='mb-5'>
+								<span className='font-bold'>Freelancers: </span>  Accurate billing and lots of
 								payment options get you paid faster and let clients pay how they
 								like. Recurring invoices, Automated Payment Reminders, and
 								Online Payments make it easy.
 							</p>
 
-							<p>
-								Self-employed Professionals: Payments are all consolidated in
+							<p className='mb-5'>
+                <span className='font-bold'>Self-employed Professionals: </span> 
+								 Payments are all consolidated in
 								one place, and everything is organized and easy to track Small
-								Businesses: Bookkeeping and tax filing become easy by
-								highlighting all transactions with discrepancies. Accountants:
-								Auditing accounts for Businesses become easy as invoice matching
-								becomes easy with our artificial intelligence bot.
 							</p>
+              <p className='mb-5'><span className='font-bold'>Businesses: </span>
+                 Bookkeeping and tax filing become easy by
+								highlighting all transactions with discrepancies.</p>
+                <p className='mb-5'><span className='font-bold'> Accountants:</span>
+								Auditing accounts for Businesses become easy as invoice matching
+								becomes easy with our artificial intelligence bot.</p>
 						</section>
+					
 						<section className="mb-11">
-							<h1 className="text-2xl mb-4"> How to use Recon</h1>
-							<p>
-								Welcome, aboard! We’re glad that you’ve chosen AccountPal for
+							<h1 className="font-bold text-2xl mb-4"> How to use Recon</h1>
+							<p className='mb-6'>
+								Welcome, aboard! We’re glad that you’ve chosen Recon for
 								your account statement matching. Let’s get you started right
 								away on how to take advantage of our platform to make things
 								easy.
 							</p>
-							<h2 className="text-[#194185] mb-2">Step one</h2>
-							<p>
+							<h2 className="text-[#194185] text-xl	 font-bold mb-2">Step one</h2>
+							<p className='mb-5'>
 								From your Dashboard upload your bank statement by clicking on
 								the “Browse Files” button and choosing the
 								CSV(comma-separated-document) file format of your bank
 								statement.
 							</p>
-              <img src={stepOne} alt= ' ' />
+              <img className='mb-5' src={stepOne} alt= ' ' />
 
-							<h2 className="text-[#194185] mb-2">Step Two</h2>
-							<p>
+							<h2 className="text-[#194185] text-xl	 font-bold mb-2">Step Two</h2>
+							<p className='mb-5'>
 								From your Dashboard upload your Sales Records from Enterprise
 								resource planning (ERP)
 							</p>
-              <img src={stepTwo} alt= ' ' />
-							<h2 className="text-[#194185] mb-2">Step Three</h2>
-							<p>
+              <img className='mb-5' src={stepTwo} alt= ' ' />
+							<h2 className="text-[#194185] text-xl	 font-bold mb-2">Step Three</h2>
+							<p className='mb-5'>
 								On your “uploaded sale and account ready” page click on
 								reconcile button.
 							</p>
-              <img src={stepThree} alt= ' ' />
-							<h2>Step four</h2>
-							<p>
+              <img className='mb-5' src={stepThree} alt= ' ' />
+							<h2 className="text-[#194185] text-xl	 font-bold mb-2">Step four</h2>
+							<p className='mb-5'>
 								After the reconciliation process is complete, click Download
 								Button to Download your report in PDF or Spreadsheet format.
 							</p>
 						</section>
 						<section>
-							<h1 className=' ' >Key Values Recon offers.</h1>
+							<h1 className='font-bold  text-2xl mb-4' >Key Features.</h1>
+							<h2 className='font-bold   mb-4' >Key Values Recon offers.</h2>
 
-							<p>
-								Automatic Matching System: Account Pal uses an Artificial
+              <ul className='list-disc ml-4'>
+							<li>
+                <span className='font-bold'>Automatic Matching System: </span>
+								 Recon uses an Artificial
 								Intelligence bot to request detailed account statements from
 								your bank at the close of business and automatically performs a
 								3-way match of the documents from the different departments.
-								Easy Data Entry: Account Pal offers an easy and automatic
+                
+							</li>
+              <li><span className='font-bold'>Easy Data Entry: </span>
+								 Recon offers an easy and automatic
 								invoice data entry for the suppliers and the client. Creating a
-								close touchless invoice process. Easy Integration into ERPs:
+								close touchless invoice process.</li>
+                <li>
+                  <span className='font-bold'>Easy Integration into ERPs: </span>
 								Easy Integration with the company’s Enterprise resource planning
 								(ERP) systems like Sage Intacct, Microsoft Dynamics, SAP, and
-								Oracle. Vendor Habits Prediction: Account Pal Artificial
+								Oracle.
+                </li>
+                <li>
+                  <span className='font-bold'>Vendor Habits Prediction: </span>
+                  Recon Artificial
 								Intelligence bot helps ensure accuracy and learn vendor
 								tendencies by mapping past transactions with new ones and
 								highlighting discrepancies.
-							</p>
+                </li>
+
+              </ul>
 						</section>
 					</div>
 				</div>
