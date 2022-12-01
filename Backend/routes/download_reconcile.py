b36a5d4bf20a2315a8494b2f552f509da63495ad
Oluwabunmi Fife <oluwabunmifife@gmail.com>
2022-12-01 16:00:02 +0100

    feat(BAE-41): Reworked the download function. It now downloads as pdf

--- Backend/routes/download_reconcile.py
+++ Backend/routes/download_reconcile.py
@@ -1,19 +1,22 @@
 import requests as req
 from fastapi import APIRouter
-from fastapi.responses import FileResponse
+import pandas as pd
+import pdfkit
 
 router = APIRouter()
 
-# @router.get("/download_reconcile")
-# def download_reconcile(file_path):
-#     if os.path.isfile(file_path):
-#         return FileResponse(file_path, media_type="application/octet-stream", filename="reconciled.json")
-#         # return FileResponse(path=file_path)
-#     return None
 
 
 @router.get("/download_reconcile")
 def download_reconcile():
-    with req.get('https://salesaccount-web-hng.vercel.app/dashboard/accountreport') as rq:
-        with open('reconciled.csv', 'wb') as file:
-            file.write(rq.content)
+    with req.get('http://bit.ly/ofac_sanctions_csv') as rq:
+        
+        #This should read the file exactly as it is from the url and download it as pdf
+        pdfkit.from_url('https://salesaccount-web-hng.vercel.app/dashboard/accountreport', 'reconciled.pdf')
+
+
+
+
+#NOTE: pip install pandas and pdfkit
+# Also install wkhtmltopdf from https://wkhtmltopdf.org/downloads.html
+# Add the path to the bin folder to your environment variables 
