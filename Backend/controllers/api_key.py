04e113bfe1c58d41a7b71267318f04e4fc36529c
lone-wolve <ai21821021@utg.edu.gm>
2022-12-02 22:13:37 +0000

    changes made and conflict resolved

--- Backend/controllers/api_key.py
+++ Backend/controllers/api_key.py
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
 API_KEY = "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"
-=======
-#!/usr/bin/python3
-""" OPENAI API KEY """
-
-API_KEY = "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"
->>>>>>> f2c83989207b07cda10b8a17dfa6ffd3e4247757
