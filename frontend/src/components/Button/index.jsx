8f2cfd762c32a86db74add2fcbe6307a8c1773af
Adeolaking <adeolakinga@gmail.com>
2022-12-02 12:08:28 +0100

    responsiveness fixes

--- frontend/src/components/Button/index.jsx
+++ frontend/src/components/Button/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Button = ({ path, isPrimary, title }) => (
 	<a
 		href={path}
-		className={`rounded-md py-[12px] px-[20px] cursor-pointer text-[18px] ${
+		className={`rounded-md py-[12px] px-[20px] inline-block min-w-max cursor-pointer text-[18px] ${
 			isPrimary
 				? 'bg-[#FFFFFF}] text-[#1570EF] border border-[#1570EF]'
 				: 'bg-[#2E90FA] text-white'
