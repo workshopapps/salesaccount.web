cf32ccad63e85252b1d04efae5141f2196c2af4f
lone-wolve <ai21821021@utg.edu.gm>
2022-12-02 13:02:33 +0000

    feat(BAE-46)Use GPT-3 to help match two uploaded file Account Stataement and Sales Record by feeding the ai model it with text of both files

--- Backend/controllers/matching.py
+++ Backend/controllers/matching.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """ DOCUMENT MATCHING MODULE """
-import datasist as ds
-from fuzzywuzzy import fuzz, process
-from .conversion import file_conversion
-import pandas as pd
-import json
+import os
+import openai
 
+from .conversion import file_conversion
+from .api_key import API_KEY
+import pandas as pd
+import json,csv,io
+
+
+openai.api_key = API_KEY
 
 def match(account_statement, financial_record):
 		""" Matches similar transactions in the documents
@@ -17,69 +21,27 @@ def match(account_statement, financial_record):
 		Return:
 		object: json
 		"""
+		keyword = "Below are two files Account Statement and Sales Record\nReconcile both Account Statement and Sales Record \n\n\n"
+
 		statement_table = pd.read_json(file_conversion(account_statement))
+		statement_csv = statement_table.to_csv()
 		records_table = pd.read_json(file_conversion(financial_record))
-		statement_table.columns = statement_table.columns.str.strip()
-		records_table.columns = records_table.columns.str.strip()
-		statement_table["balance"]=statement_table["balance"].str.replace(",","")
-		statement_table["debit"] = statement_table["debit"].str.replace(",","")
-		statement_table["credit"] = statement_table["credit"].str.replace(",","")
-		statement_table[["balance", "debit", "credit"]] = statement_table[["balance", "debit", "credit"]].astype("float")
-		statement_table['Date']= pd.to_datetime(statement_table['Date'])
-		records_table["Price"] = records_table["Price"].str.replace(",", "")
-		records_table["Price"] = records_table["Price"].astype("float")
-		records_table["Best Match"]= records_table["Description"].map(
-										lambda x: process.extractOne(
-											x, statement_table["Description"]
-										)[0])
-		output = records_table.merge(statement_table,
-									 left_on="Best Match",
-									 right_on="Description",
-									 how="outer",
-									 indicator=True)
-		output["difference"] = output["Price"] - output["credit"]#difference need to be flexible
-		output = output.drop(columns =['balance', "debit"], axis=1)
-		output = output.sort_values(by=['Date'])
-		result = output.to_json(orient='records')
-		parsed = json.loads(result)
-		response = json.dumps(parsed, indent=4)
-		return response
+		records_csv = records_table.to_csv()
 
 
+		prompt = f"{keyword}{statement_csv}\n\n\n{records_csv}"
 
-def clean_statement(filepath):
-    df = pd.read_csv(filepath)
-    df.columns=df.columns.str.strip()
-    df["Balance"]=df["Balance"].str.replace(",","")
-    df["Money out"] = df["Money out"].str.replace(",","")
-    df["Money in"] = df["Money in"].str.replace(",","")
-    df[["Balance", "Money out", "Money in"]] = df[["Balance", "Money out", "Money in"]].astype("float")
-    df['Date']= pd.to_datetime(df['Date'])
-    
-    return df
+		response = openai.Completion.create(
+  					model="text-davinci-003",
+  					prompt = prompt ,
+					temperature=0.25,
+					max_tokens=1257,
+					top_p=1,
+					frequency_penalty=0,
+					presence_penalty=0
+					)
+		string = response.choices[0].text
+		# reader = csv.DictReader(io.StringIO(string))
 
-def clean_record(filepath):
-    output = pd.read_csv(filepath)
-    output.columns = output.columns.str.strip()
-    output["Price"] = output["Price"].str.replace(",","")
-    output["Price"] = output["Price"].astype("float")
-    
-    return output
 
-def reconcile(sales_record, account_statement):
-    sales_record["Best Match"]=sales_record["Description"].map(
-        lambda x: process.extractOne(
-            x, account_statement["Description"]
-        )[0]
-    )
-    
-    output = sales_record.merge(
-        account_statement,
-        left_on="Best Match",
-        right_on="Description",
-        how="outer",
-        indicator=False
-    )
-    output["difference"] = output["Price"] - output["Money in"]
-    output = output.drop(columns =['Balance', "Money out"], axis=1)
-    return output
+		return json.dumps(string)
