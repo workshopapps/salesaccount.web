4ff9cb80f667d0eeb25c829a8bda7c03af598b80
earlpappi <earlpappi@gmail.com>
2022-12-02 23:20:14 +0100

    Revert "--commits: header, footer, about us -- Fixes"
    This reverts commit 0d84d4e8f22f6b305a994d9bc6f8c8cdc10af8ee.

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -6,29 +6,25 @@ import logoUpdate from '../../assets/logoUpdate.svg';
 
 const aboutUsData = [
 	{
-		name: 'Our Mission',
+		name: 'Our Story',
 		path: '/history',
 	},
 	{
-		name: 'Our Vision',
+		name: 'Work Environment',
 		path: '/teams',
 	},
 	{
-		name: 'Our Core Values',
+		name: 'Email Us',
 		path: '/contact',
 	},
 ];
 const resourcesData = [
 	{
+		name: 'Help Center',
+		path: '/privacyandhelp',
+	},
+	{
 		name: 'Support',
-		path: '/privacyandhelp',
-	},
-	{
-		name: 'Privacy',
-		path: '/privacyandhelp',
-	},
-	{
-		name: 'Blog',
 		path: '/privacyandhelp',
 	},
 	{
@@ -36,66 +32,81 @@ const resourcesData = [
 		path: '/faqs',
 	},
 ];
-
+const ProductData = [
+	{
+		name: 'Pricing',
+		path: '/pricing',
+	},
+	{
+		name: 'How it works',
+		path: '/guides',
+	},
+	{
+		name: 'Features',
+		path: '/resources',
+	},
+];
 
 const Footer = () => (
-	<footer className="bg-[#D1E9FF] py-[12em]">
-		<div className='flex flex-col md:flex-row justify-between w-9/12 mx-auto'>
-
-
-			<div>
-				<figure className='flex flex-col max-md:max-w-[275px] items-start'>
-					<img className='w-10/12' src={logoUpdate} alt="Company logo"/>					
-				</figure>
-				
-				<div className="text-[#98A2B3] flex items-center gap-6 mt-6">
-					<BsTwitter size={24} />
-					<BsFacebook size={24} />
-					<BsInstagram size={24} />
-				</div>
+	<footer className="bg-[#D1E9FF] w-full flex flex-col md:flex-row justify-between py-[50px] xl:py-[100px] xl:px-[100px] px-[44px]">
+		<div>
+			<figure className='flex flex-col max-md:max-w-[275px]'>
+			<img className='w-full' src={logoUpdate}  alt="Company logo" />
+			{/* <p className='font-semibold text-[40px] leading-[50px]'>reconcile.<span className='text-[#2E90FA]'>AI</span></p> */}
+			</figure>
+			{/* <div className="pt-[4px] mb-[18px] w-[230px]">
+				<p className="text-[#344054] font-normal text-[16px] xl:text-[20px]">
+					This is a website that can help you balance your sales account
+				</p>
+			</div> removed According to new design */ }
+			<div className="text-[#98A2B3] flex items-center gap-6 mt-6">
+				<BsTwitter size={32} />
+				<BsFacebook size={32} />
+				<BsInstagram size={32} />
 			</div>
-
-			{/* About us Tab */}
-			<div className="flex flex-col pt-[10px] xl:pt-0">
-				<h3 className="font-medium text-[#344054] md:text-[1.8em]">
-					About Us
-				</h3>
-
-				<div className='flex flex-col gap-6 mt-6'>
-					{aboutUsData.map((item) => (
-						<Link
-							key={uuid()}
-							to={item.path}
-							className="text-[#344054] md:text-[1.4em]"
-						>
-							{item.name}
-						</Link>
-					))}
-				</div>
-			</div>
-
-			{/* Resources Tab */}
-			<div className="flex flex-col pt-[10px] xl:pt-0">
-				<h3 className="font-medium text-[#344054] md:text-[1.8em]">
-					Resources
-				</h3>
-
-				<div className='flex flex-col gap-6 mt-6'>
-					{resourcesData.map((item) => (
-						<Link
-							key={uuid()}
-							to={item.path}
-							className="text-[#344054] md:text-[1.4em]"
-						>
-							{item.name}
-						</Link>
-					))}
-				</div>
-			</div>
-
 		</div>
-
-
+		<div className="flex flex-col pt-[10px] xl:pt-0">
+			<h3 className="font-medium text-[#344054] text-[18px] xl:text-[28px]">
+				About Us
+			</h3>
+			{aboutUsData.map((item) => (
+				<Link
+					to={item.path}
+					key={uuid()}
+					className="text-[#344054] font-normal  text-[16px] xl:text-[20px] py-[4px]"
+				>
+					{item.name}
+				</Link>
+			))}
+		</div>
+		<div className="flex flex-col pt-[10px] xl:pt-0">
+			<h3 className="font-medium text-[#344054] text-[18px] xl:text-[28px]">
+				Resources
+			</h3>
+			{resourcesData.map((item) => (
+				<Link
+					key={uuid()}
+					to={item.path}
+					className="text-[#344054] font-normal text-[16px] xl:text-[20px] py-[4px]"
+				>
+					{item.name}
+				</Link>
+			))}
+		</div>
+		<div className="flex flex-col pt-[10px] xl:pt-0">
+			<h3 className="font-medium text-[#344054] text-[18px] xl:text-[28px]">
+				Product
+			</h3>
+			{ProductData.map((item) => (
+				<Link
+					to={item.path}
+					key={uuid()}
+					className="text-[#344054] font-normal text-[16px] xl:text-[20px] py-[4px]"
+				>
+					{item.name}
+				</Link>
+			))}
+		</div>
 	</footer>
 );
 
--- frontend/src/pages/AboutUs/index.jsx
+++ frontend/src/pages/AboutUs/index.jsx
@@ -1,10 +1,10 @@
 // import React from 'react';
 import Footer from '../../components/Footer';
-import heroImg from '../../assets/images/about-us/hero-img.svg';
-import vision from '../../assets/images/about-us/vision.svg';
-import blueSquare from '../../assets/images/about-us/blue-square.svg';
-import mission from '../../assets/images/about-us/mission.svg';
-import valueImg from '../../assets/images/about-us/values.svg';
+// import heroImg from '../../assets/images/about-us/hero-img.svg';
+// import vision from '../../assets/images/about-us/vision.svg';
+// import blueSquare from '../../assets/images/about-us/blue-square.svg';
+// import mission from '../../assets/images/about-us/mission.svg';
+// import valueImg from '../../assets/images/about-us/values.svg';
 import NavigationBar from '../../components/NavigationBar';
 
 const AboutUs = () => (
@@ -16,7 +16,7 @@ const AboutUs = () => (
 					<h1 className="text-[#101828] lg:mb-4  font-semibold text-center md:font-bold md:text-5xl md:text-left  md:mb-5">
 						Who we are?
 					</h1>
-					<p className="text-sm font-normal text-[#101828] text-left lg:mb-[38px] mb-4 mt-4 md:text-left md:mb-0 md:h-[180px] md:w-[535px] md:text-xl">
+					<p className="text-sm font-normal text-[#101828] text-center lg:mb-[38px] mb-4 mt-4 md:text-center md:mb-0 md:h-[180px] md:w-[535px] md:text-xl">
 						We are a team of professionals whose aim is to give you and amazing
 						user experience. We are not only professionals but a family where
 						everybody works together with each other&apos;s interest of growth
@@ -24,7 +24,7 @@ const AboutUs = () => (
 					</p>
 				</div>
 				<div className=" relative mt-8 lg:mt-0 z-10">
-					<img src={heroImg} alt="Hero" className="" />
+					{/* <img src={heroImg} alt="Hero" className="" />
 
 					<img
 						className=" absolute -z-10 -top-[2rem]  -right-[2rem] hidden lg:block "
@@ -35,7 +35,7 @@ const AboutUs = () => (
 						className="absolute -z-10 -ml-[2rem] -mt-[5rem] hidden lg:block "
 						src={blueSquare}
 						alt="blue-square"
-					/>
+					/> */}
 				</div>
 			</div>
 		</section>
@@ -51,7 +51,7 @@ const AboutUs = () => (
 						To make easy sales and accounting reconciliation across all business
 						types.
 					</p>
-					<img
+					{/* <img
 						className="absolute top-[7rem] -left-[24px] -z-10 hidden lg:block"
 						src={blueSquare}
 						alt=""
@@ -60,15 +60,15 @@ const AboutUs = () => (
 						className="absolute -bottom-[1.5rem] -right-[22px] -z-10 hidden lg:block"
 						src={blueSquare}
 						alt=""
-					/>
+					/> */}
 				</div>
 
 				<div>
-					<img
+					{/* <img
 						className="lg:ml-[36px] lg:mt-0 mt-4"
 						src={vision}
 						alt="People holding hands in agreement"
-					/>
+					/> */}
 				</div>
 			</div>
 		</section>
@@ -84,7 +84,7 @@ const AboutUs = () => (
 							through the use of technology enabled solutions and making them
 							easily accessible for businesses.
 						</p>
-						<img
+						{/* <img
 							className="absolute -z-10 top-[11.5rem] -left-[28px] hidden lg:block"
 							src={blueSquare}
 							alt=""
@@ -93,14 +93,14 @@ const AboutUs = () => (
 							className="absolute -bottom-[1.5rem] -right-[28px]  -z-10 hidden lg:block"
 							src={blueSquare}
 							alt=""
-						/>
+						/> */}
 					</div>
 				</div>
-				<img
+				{/* <img
 					className="lg:mt-12 mt-4"
 					src={mission}
 					alt="Person working and writing on a notebook"
-				/>
+				/> */}
 			</div>
 		</section>
 		<section className=" lg:pl-36 lg:pr-36 lg:pb-20 lg:pt-10 z-0 p-8 ">
@@ -130,23 +130,23 @@ const AboutUs = () => (
 						</ul>
 					</div>
 
-					<img
+					{/* <img
 						className="absolute top-[7.5rem] -left-[24px] -z-10 hidden lg:block"
 						src={blueSquare}
 						alt=""
-					/>
-					<img
+					/> */}
+					{/* <img
 						className="absolute -bottom-[1.5rem] -right-[22px] -z-10 hidden lg:block"
 						src={blueSquare}
 						alt=""
-					/>
+					/> */}
 				</div>
 				<div>
-					<img
+					{/* <img
 						className="lg:ml-8 lg:mt-16 mt-4"
 						src={valueImg}
 						alt="A hand building up small bricks"
-					/>
+					/> */}
 				</div>
 			</div>
 		</section>
