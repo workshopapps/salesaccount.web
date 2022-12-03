01641921559dc13eeaddd89b2496b330f34ffefa
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-12-03 22:16:02 +0100

    New FAQ page

--- frontend/src/components/Faqs/faqArray.js
+++ frontend/src/components/Faqs/faqArray.js
@@ -1,76 +1,77 @@
 const faqArray = [
 	{
 		id: 0,
-		question: 'What is Account Pal and how does it work?',
-		answer:
-			'Account pal is a web application that compares third-party and independent financial statements and records with internal financial records and ledgers. With the use of an AI Model and GPT -3, Account Pal automatically makes the accountant’s job easy and avoids errors. Account Pal automates tasks fast and easy for users who want to validate all transactions for the day, confirm payments and compare sales with the closing account statement.',
+		title: 'What is ReconcileAI and how does it work?',
+		content:
+			'ReconcileAI is a web application that compares third-party and independent financial statements and records with internal financial records and ledgers. With the use of an AI Model and GPT -3, ReconcileAI automatically makes the accountant’s job easy and avoids errors. ReconcileAI automates tasks fast and easy for users who want to validate all transactions for the day, confirm payments and compare sales with the closing account statement.',
 	},
 	{
 		id: 1,
-		question:
+		title:
 			'Why should I make use of Account Pal to match my account statement to sales?',
-		answer:
-			'Account Pal for SMES and SMBs saves time that would be spent if matching accounts to sales were done manually. With this web application, you can be sure to reduce human errors and fradulent risks associated with account reconciliation. Account Pal also ensures seamless and fast automation for users. We have put in place an amzing feature which allows users to track periodic net balance.',
+		content:
+			'ReconcileAI for SMES and SMBs saves time that would be spent if matching accounts to sales were done manually. With this web application, you can be sure to reduce human errors and fradulent risks associated with account reconciliation. ReconcileAI also ensures seamless and fast automation for users. We have put in place an amzing feature which allows users to track periodic net balance.',
 	},
 	{
 		id: 2,
-		question: 'How do I receive my report?',
-		answer:
-			'On our Account Pal website, users can simply attach sales and bank statement files in a PDF file. With files uploaded, the document will be processed and returns with a file that contains the analysed and reconciled. The result can be downloaded as requested, or viewed right here on our website.',
+		title: 'How do I receive my report?',
+		content:
+			'On our ReconcileAI website, users can simply attach sales and bank statement files in a PDF file. With files uploaded, the document will be processed and returns with a file that contains the analysed and reconciled. The result can be downloaded as requested, or viewed right here on our website.',
 	},
 	{
 		id: 3,
-		question: 'Is Account Pal free for first time users?',
-		answer:
-			'Account Pal has a 1-month trial period. When it expires, you can continue using the website for a small subscription fee.',
+		title: 'Is ReconcileAI free for first time users?',
+		content:
+			'ReconcileAI has a 1-month trial period. When it expires, you can continue using the website for a small subscription fee.',
 	},
 	{
 		id: 4,
-		question:
+		title:
 			'Is there a cap on the maximum number of transactions I can do daily, weekly or monthly?',
-		answer:
-			'There is no cap on on the number of transactions one can carry out using Account Pal.',
+		content:
+			'There is no cap on on the number of transactions one can carry out using ReconcileAI.',
 	},
 	{
 		id: 5,
-		question: 'Can my accountants and I collaborate on Account Pal?',
-		answer:
-			'Yes, you can collaborate with others on your Account Pal by sharing your sign-in details with them.',
+		title: 'Can my accountants and I collaborate on ReconcileAI?',
+		content:
+			'Yes, you can collaborate with others on your ReconcileAI by sharing your sign-in details with them.',
 	},
 	{
 		id: 6,
-		question: 'Can I access Account Pal on of my devices?',
-		answer:
-			'Account Pal can be used on your PC and mobile devices. You only need to sign in using the device of your choice.',
+		title: 'Can I access ReconcileAI on of my devices?',
+		content:
+			'ReconcileAI can be used on your PC and mobile devices. You only need to sign in using the device of your choice.',
 	},
 	{
 		id: 7,
-		question: 'Can I attach multiple files to a single transaction?',
-		answer:
-			'Yes, Account Pal allows for upload of multiple files per transaction as needed.',
+		title: 'Can I attach multiple files to a single transaction?',
+		content:
+			'Yes, ReconcileAI allows for upload of multiple files per transaction as needed.',
 	},
 	{
 		id: 8,
-		question: 'What are the advantages of using Account Pal?',
-		answer:
-			'Account Pal has multiple features to help you with smooth financial transactions.',
+		title: 'What are the advantages of using ReconcileAI?',
+		content:
+			'ReconcileAI has multiple features to help you with smooth financial transactions.',
 	},
 	{
 		id: 9,
-		question: 'Can everyone view my my report/account?',
-		answer:
+		title: 'Can everyone view my my report/account?',
+		content:
 			'The details in your account are your private information and can only be shared by you, unless access is granted to your collaborators..',
 	},
 	{
 		id: 10,
-		question: 'What are account statements?',
-		answer:
+		title: 'What are account statements?',
+		content:
 			'Account statements are periodic summaries of account activity within a defined period',
 	},
 	{
 		id: 11,
-		question: 'How do I get my income statement?',
-		answer: 'Account Pal provides you with your income statement upon request.',
+		title: 'How do I get my income statement?',
+		content:
+			'ReconcileAI provides you with your income statement upon request.',
 	},
 ];
 
