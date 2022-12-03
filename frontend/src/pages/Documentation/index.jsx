aec293773a6a15e9539caad1090a90484d36b8b5
Kacey <53833032+kelech1@users.noreply.github.com>
2022-12-03 12:18:55 +0000

    resolved screenshots issues

--- frontend/src/pages/Documentation/index.jsx
+++ frontend/src/pages/Documentation/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import NavigationBar from '../../components/NavigationBar';
 import stepOne from '../../assets/images/documentation/stepone.png';
+import steponeMobile from '../../assets/images/documentation/steponeMobile.png'
+import steptwoMobile from '../../assets/images/documentation/steptwoMobile.png'
+import stepthreeMobile from '../../assets/images/documentation/stepthreeMobile.png'
 import stepTwo from '../../assets/images/documentation/steptwo.png';
 import stepThree from '../../assets/images/documentation/stepthree.png';
 import Footer from '../../components/Footer';
@@ -8,13 +11,13 @@ import Footer from '../../components/Footer';
 const Documentation = () => (
 	<div className="text-gray-800 bg-white ">
 		<NavigationBar />
-		<header className="header bg-[#D7DBDF] text-[#101828] shadow py-4 px-4">
+		<header className="header bg-[#2E90FA] md:bg-[#D7DBDF] text-white md:text-[#101828] shadow py-4 px-4">
 
 
-			<div className="flex flex-row items-center justify-center h-40 header-content">
+			<div className="flex flex-row items-center justify-center md:h-40 header-content">
 
-				<h1 className="text-3xl text-center">
-					A step by step guide on how <br /> to use Reconcile.<span className='text-[#2E90FA]'>AI</span>
+				<h1 className="text-sm text-center md:text-3xl">
+					A step by step guide on how <br /> to use Reconcile.<span className='md:text-[#2E90FA]'>AI</span>
 				</h1>
 			</div>
 		</header>
@@ -52,15 +55,15 @@ const Documentation = () => (
 					</ul>
 				</div>
 			</aside>
-			<main className="main flex flex-col flex-grow  md:ml-0 p-[3rem] transition-all duration-150 ease-in h-[100vh] overflow-scroll">
+			<main className="main flex flex-col flex-grow  md:ml-0 p-3 md:p-[3rem] transition-all duration-150 ease-in h-[100vh] overflow-scroll">
 
-				<div className="flex flex-col flex-grow p-4 main-content">
+				<div className="flex flex-col flex-grow p-2 md:p-4 main-content">
 					<div className="flex flex-col flex-grow mt-4 rounded">
 						<section className="mb-11"  id='intro'>
-							<h1 className="mb-4 text-2xl font-bold ">Introduction</h1>
+							<h1 className="mb-4 text-base font-bold md:text-2xl ">Introduction</h1>
 
               
-							<p className='mb-5'>
+							<p className='mb-5 text-xs text-[#667085] md:text-lg'>
 								Reconcile.<span className='text-[#2E90FA]'>AI</span> is a web application that compares third-party and
 								independent financial statements and records with internal
 								financial records and ledgers. It does this accurately with
@@ -71,100 +74,112 @@ const Documentation = () => (
 								very reliable for Small and Medium Enterprises.
 							</p>
 
-              <h1 className="mb-4 text-2xl font-bold">Who It&apos;s For?</h1>
+              				<h1 className="mb-4 text-base font-bold md:text-2xl">Who It&apos;s For?</h1>
 							
 
-							<p className='mb-5'>
+							<p className='mb-5 text-xs text-[#667085] md:text-lg'>
 								<span className='font-bold'>Freelancers: </span>  Accurate billing and lots of
 								payment options get you paid faster and let clients pay how they
 								like. Recurring invoices, Automated Payment Reminders, and
 								Online Payments make it easy.
 							</p>
 
-							<p className='mb-5'>
-                <span className='font-bold'>Self-employed Professionals: </span> 
+							<p className='mb-5 text-xs text-[#667085] md:text-lg'>
+                			<span className='font-bold'>Self-employed Professionals: </span> 
 								 Payments are all consolidated in
 								one place, and everything is organized and easy to track Small
 							</p>
-              <p className='mb-5'><span className='font-bold'>Businesses: </span>
-                 Bookkeeping and tax filing become easy by
+             		 		<p className='mb-5 text-xs text-[#667085] md:text-lg'><span className='font-bold'>Businesses: </span>
+                 				Bookkeeping and tax filing become easy by
 								highlighting all transactions with discrepancies.</p>
-                <p className='mb-5'><span className='font-bold'> Accountants:</span>
+							<p className='mb-5 text-xs text-[#667085] md:text-lg'><span className='font-bold'> Accountants:</span>
 								Auditing accounts for Businesses become easy as invoice matching
 								becomes easy with our artificial intelligence bot.</p>
 						</section>
 					
 						<section className="mb-11" id='howToUse'>
 
-							<h1 className="mb-4 text-2xl font-bold"> How to use Reconcile.<span className='text-[#2E90FA]'>AI</span></h1>
+							<h1 className="mb-4 text-base font-bold md:text-2xl"> How to use Reconcile.<span className='text-[#2E90FA]'>AI</span></h1>
 
-							<p className='mb-6'>
+							<p className='mb-6 text-xs text-[#667085] md:text-lg'>
 								Welcome, aboard! We’re glad that you’ve chosen Reconcile.<span className='text-[#2E90FA]'>AI</span> for
 								your account statement matching. Let’s get you started right
 								away on how to take advantage of our platform to make things
 								easy.
 							</p>
-							<h2 className="text-[#194185] text-xl	 font-bold mb-2">Step one</h2>
-							<p className='mb-5'>
+							<h2 className="text-[#194185] mb-2 text-base text-center md:text-start font-bold md:text-xl">Step One</h2>
+							<p className='mb-5 text-xs text-[#101828] text-center md:text-start md:text-lg'>
 								From your Dashboard upload your bank statement by clicking on
 								the “Browse Files” button and choosing the
 								CSV(comma-separated-document) file format of your bank
 								statement.
 							</p>
-              <img className='mb-5' src={stepOne} alt= ' ' />
 
-							<h2 className="text-[#194185] text-xl	 font-bold mb-2">Step Two</h2>
-							<p className='mb-5'>
+							<picture >
+							<img className='hidden mb-5 md:inline' src ={stepOne} alt= ' ' />
+							<img className='inline-block  mx-[76px] mb-5 md:hidden' src ={steponeMobile} alt= ' '/>
+							</picture>
+              				
+
+							<h2 className="text-[#194185] mb-2 text-base text-center md:text-start font-bold md:text-xl">Step Two</h2>
+							<p className='mb-5 text-xs text-[#101828] text-center md:text-start md:text-lg'>
 								From your Dashboard upload your Sales Records from Enterprise
 								resource planning (ERP)
 							</p>
-              <img className='mb-5 w-[100%]' src={stepTwo} alt= ' ' />
-							<h2 className="text-[#194185] text-xl	 font-bold mb-2">Step Three</h2>
-							<p className='mb-5'>
+							<picture >
+							<img className='hidden mb-5 md:inline' src ={stepTwo} alt= ' ' />
+							<img className='inline-block  mx-[76px] mb-5 md:hidden' src ={steptwoMobile} alt= ' '/>
+							</picture>
+							<h2 className="text-[#194185] mb-2 text-base text-center md:text-start font-bold md:text-xl">Step Three</h2>
+							<p className='mb-5 text-xs text-[#101828] text-center md:text-start md:text-lg'>
 								On your “uploaded sale and account ready” page click on
-								reconcile.<span className='text-[#2E90FA]'>AI</span>cile button.
+								reconcile button.
 							</p>
-              <img className='mb-5 w-[100%]' src={stepThree} alt= ' ' />
-							<h2 className="text-[#194185] text-xl	 font-bold mb-2">Step four</h2>
-							<p className='mb-5'>
+							<picture >
+							<img className='hidden mb-5 md:inline' src ={stepThree} alt= ' ' />
+							<img className='inline-block  mx-[76px] mb-5 md:hidden' src ={stepthreeMobile} alt= ' '/>
+							</picture>
+							<h2 className="text-[#194185] text-center md:text-start mb-2 text-base font-bold md:text-xl">Step Four</h2>
+							<p className='mb-5 text-xs text-[#101828] text-center md:text-start md:text-lg'>
 								After the reconcile.<span className='text-[#2E90FA]'>AI</span> process is complete, click Download
 								Button to Download your report in PDF or Spreadsheet format.
 							</p>
 						</section>
 						<section id='keyFeatures'>
 
-							<h1 className='mb-4 text-2xl font-bold' >Key Features.</h1>
-							<h2 className='mb-4 font-bold' >Key Values Reconcile.<span className='text-[#2E90FA]'>AI</span> offers.</h2>
+							<h1 className='mb-4 text-base font-bold md:text-2xl' >Key Features.</h1>
+							<h2 className='mb-5 text-xs text-[#667085] font-bold md:text-lg' >Key Values Reconcile.<span className='text-[#2E90FA]'>AI</span> offers.</h2>
 
 
-              <ul className='ml-4 list-disc'>
-							<li>
-                <span className='font-bold'>Automatic Matching System: </span>
-								 Reconcile.<span className='text-[#2E90FA]'>AI</span> uses an Artificial
-								Intelligence bot to request detailed account statements from
-								your bank at the close of business and automatically performs a
-								3-way match of the documents from the different departments.
-                
-							</li>
-              <li><span className='font-bold'>Easy Data Entry: </span>
-								 Reconcile.<span className='text-[#2E90FA]'>AI</span> offers an easy and automatic
-								invoice data entry for the suppliers and the client. Creating a
-								close touchless invoice process.</li>
-                <li>
-                  <span className='font-bold'>Easy Integration into ERPs: </span>
-								Easy Integration with the company’s Enterprise resource planning
-								(ERP) systems like Sage Intacct, Microsoft Dynamics, SAP, and
-								Oracle.
-                </li>
-                <li>
-                  <span className='font-bold'>Vendor Habits Prediction: </span>
-                  Reconcile.<span className='text-[#2E90FA]'>AI</span> Artificial
-								Intelligence bot helps ensure accuracy and learn vendor
-								tendencies by mapping past transactions with new ones and
-								highlighting discrepancies.
-                </li>
+							<ul className='ml-4 text-xs text-[#667085] list-disc md:text-lg'>
+								<li>
+									<span className='font-bold'>Automatic Matching System: </span>
+									Reconcile.<span className='text-[#2E90FA]'>AI</span> uses an Artificial
+									Intelligence bot to request detailed account statements from
+									your bank at the close of business and automatically performs a
+									3-way match of the documents from the different departments.
+					
+								</li>
+								<li>
+									<span className='font-bold'>Easy Data Entry: </span>
+									Reconcile.<span className='text-[#2E90FA]'>AI</span> offers an easy and automatic
+									invoice data entry for the suppliers and the client. Creating a
+									close touchless invoice process.</li>
+								<li>
+									<span className='font-bold'>Easy Integration into ERPs: </span>
+									Easy Integration with the company’s Enterprise resource planning
+									(ERP) systems like Sage Intacct, Microsoft Dynamics, SAP, and
+									Oracle.
+								</li>
+								<li>
+									<span className='font-bold'>Vendor Habits Prediction: </span>
+									Reconcile.<span className='text-[#2E90FA]'>AI</span> Artificial
+									Intelligence bot helps ensure accuracy and learn vendor
+									tendencies by mapping past transactions with new ones and
+									highlighting discrepancies.
+								</li>
 
-              </ul>
+							</ul>
 						</section>
 					</div>
 				</div>
