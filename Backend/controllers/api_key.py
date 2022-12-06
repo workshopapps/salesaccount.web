4ea4bba7a4a9a2b58bc0c4b4c540689a5330e229
Oluwabunmi Fife <oluwabunmifife@gmail.com>
2022-12-06 06:15:56 +0100

    feat(BAE-43): Passed extracted text into openai function

--- Backend/controllers/api_key.py
+++ Backend/controllers/api_key.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-API_KEY = "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"
-=======
 #!/usr/bin/python3
 """ OPENAI API KEY """
 
 # API_KEY = "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"
->>>>>>> ad9a96d90ade2a421a6e6c07b10595bb34660e84
--- Backend/controllers/extract_data.py
+++ Backend/controllers/extract_data.py
@@ -1,4 +1,12 @@
+import os
+import openai
 import PyPDF2
+from .api_key import API_KEY
+
+API_KEY = os.getenv("OPENAI_API_KEY", default=API_KEY)
+openai.api_key = API_KEY # replace API_KEY with personal api secret key
+
+
 
 pdfFileObj = open('test.pdf', 'rb')
 
@@ -13,4 +21,20 @@ text = pageObj.extractText()
 with open('final.text', 'w') as file:
     file.write(text)
 
+prompt = f" Can you please extract the keywords from: " + {file}
+
 pdfFileObj.close()
+
+def openai_call(prompt=prompt):
+	""" Sends extracted text from pdf file to gpt-3
+		"""
+	response = openai.Completion.create(
+  					model="text-davinci-003",
+  					prompt = prompt,
+					temperature=0.62,
+					max_tokens=2857,
+					top_p=1,
+					frequency_penalty=0,
+					presence_penalty=0
+				)
+	return response
