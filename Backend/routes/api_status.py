2f5c91dd158f0a1ccbf8c2eb396bfec7ab0a0a86
Chidubem <nwabuisicelestine@gmail.com>
2022-12-06 16:05:05 +0100

    removed backend error used to test sentry

--- Backend/routes/api_status.py
+++ Backend/routes/api_status.py
@@ -9,5 +9,4 @@ router = APIRouter()
 @router.get("/", status_code=200)
 def api_status():
     """ Returns a json object with successful connection to the api """
-    division_by_zero = 1 / 0
     return {"message": "Connection Successful"}
