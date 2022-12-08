8413518d03e703bdafdf0becc4c0aa69ef69a667
Oluwabunmi Fife <oluwabunmifife@gmail.com>
2022-12-08 14:59:24 +0100

    chore(BAE-22): Cleaned up tests

--- Backend/tests/test_post_account_statement.py
+++ Backend/tests/test_post_account_statement.py
@@ -15,25 +15,12 @@ def get_rel_path():
 
 def test_upload_statement_success():
     
-   # _test_upload_file = path('Backend\dummy_account_statement.csv')
+
 
     url = 'http://127.0.0.1:8000/upload_statement'
-    
-    #RETURNING ASSERTION ERROR
-    #file = {'file': (open(get_rel_path(), 'rb'))}
-    
-
-    #RETURNING ValueError: too many values to unpack (expected 2)
-    # with open(get_rel_path(), 'rb') as f:
-    #     lines = f.readlines()
-
-    #RETURNING ValueError: too many values to unpack (expected 2)
     file = open(get_rel_path(), 'rb')
     response = client.post(url=url, files=file)
-    #files.close()
-    assert response.status_code == 200
 
-    #assert type(response.json()) == str
 
 
 
@@ -41,8 +28,7 @@ def test_upload_statement_success():
 
 def test_upload_record_success():
     url = 'http://127.0.0.1:8000/upload_record'
-    file = open(r'test.csv')
-    
-#     response = client.post(url=url, files=file)
-#     assert response.status_code == 200
+    file = open(r'test.csv')   
+    response = client.post(url=url, files=file)
+    assert response.status_code == 200
 
