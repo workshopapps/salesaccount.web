c99f15d368218425f8cfb7adca4af95c85378d41
Oluwabunmi Fife <oluwabunmifife@gmail.com>
2022-12-09 14:51:23 +0100

    chore(BAE-43): Cleaned up the text extraction function

--- Backend/controllers/extract_data.py
+++ Backend/controllers/extract_data.py
@@ -8,49 +8,29 @@ openai.api_key = API_KEY # replace API_KEY with personal api secret key
 
 
 
-def statement(): #Function that converts the account statement(pdf format) to string(.text)
-    pdfFileObj = open('statement.pdf', 'rb')
+# def statement(): #Function that converts the account statement(pdf format) to string(.text)
+#     pdfFileObj = open('statement.pdf', 'rb')
 
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+#     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
-    print(pdfReader.numPages)
+#     print(pdfReader.numPages)
 
-    pageObj = pdfReader.getPage(0)
+#     pageObj = pdfReader.getPage(0)
 
-    text = pageObj.extractText()
+#     text = pageObj.extractText()
 
-    with open('statement.text', 'w') as file:
-        file.write(text)
+#     with open('statement.text', 'w') as file:
+#         file.write(text)
 
-    pdfFileObj.close()
-
-
-
-def record(): #Function that converts the sales record(pdf format) to string(.text)
-    pdfFileObj = open('record.pdf', 'rb')
-
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-
-    print(pdfReader.numPages)
-
-    pageObj = pdfReader.getPage(0)
-
-    text = pageObj.extractText()
-
-    with open('record.text', 'w') as file:
-        file.write(text)
-
-    pdfFileObj.close()
-
-send = 'statement.text' + "  " + 'record.text'
+#     pdfFileObj.close()
 
 
 
 
-def openai():
+def openai(prompt): #Open-ai function
 	response = openai.Completion.create(
 	model="text-davinci-003",
-	prompt="Match all details in the files below. No title. Response in JSON  {} \n\n\nReturn the response in this format: \n[\n{\n'Date':\n 'Description':\n 'Details':\n ' Money out ':\n ' Money in ':\n ' Balance ':\n 'Item no ':\n 'Item Name':\n 'Description':\n 'Price':\n}\n]\n".format(send),
+	prompt=prompt,
 	temperature=0,
 	max_tokens=1000,
 	top_p=1,
@@ -59,3 +39,23 @@ def openai():
 	)
 
 	return response	
+
+def extract_text(filename: str): #Function that converts the sales record(pdf format) to string(.text)
+    pdfFileObj = open(filename, 'rb')
+
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+    print(pdfReader.numPages)
+
+    pageObj = pdfReader.getPage(0)
+
+    text = pageObj.extractText()
+
+    keyword = """
+        Extract just transactions details in this text below. Arrange into a table.
+        No title. Response only as a JSON inside an array\n
+        """
+    prompt = f"{keyword}\n{text}"
+
+    openai(prompt=prompt)
+    pdfFileObj.close()
