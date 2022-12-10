31b2b3c8c5f40a6b3d6e430b4fcdb939d634efaf
Chidubem <nwabuisicelestine@gmail.com>
2022-12-10 07:13:06 +0100

    changed media path

--- Backend/routes/post_documents.py
+++ Backend/routes/post_documents.py
@@ -20,13 +20,13 @@ def upload(file: UploadFile = File(...)):
     """
     try:
         contents = file.file.read()
-        with open(f"Media\{file.filename}", 'wb') as f:
+        with open(f"Media/{file.filename}", 'wb') as f:
             f.write(contents)
-        response = convert_file(f"Media\{file.filename}")
+        response = convert_file(f"Media/{file.filename}")
         null = "null"
         return eval(response)
     except Exception as e:
-        os.remove(f"Media\{file.filename}")
+        os.remove(f"Media/{file.filename}")
         return {
             "message": f"There was an error uploading the file {e}"
         }
