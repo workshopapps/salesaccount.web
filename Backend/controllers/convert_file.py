a4c70738ff48d3f069529c46467000be4e8c6b38
Joseun <ologanj@gmail.com>
2022-12-12 12:48:44 +0100

    feat(BAE 59): no header bug fixed

--- Backend/controllers/convert_file.py
+++ Backend/controllers/convert_file.py
@@ -2,10 +2,12 @@
 """ FILE CONVERSION MODULE """
 import asyncio
 import json
+import numpy as np
 import pandas as pd
 import re
 
 from .extract_text import pdf_to_text #image_to_text
+from .openai_request import openai_call
 
 
 def df_to_json(df):
@@ -22,6 +24,30 @@ def df_to_json(df):
     response = json.dumps(parsed, indent=4)
     return response
 
+
+def check_header(df, header):
+    """Converts dataframe to json object
+
+    Args:
+        df: dataframe
+        header: string containing first rows in csv
+
+    Return:
+        object: dataframe
+    """
+    keyword = """Check if the first line of a CSV file is an appropriate header.\
+            No title. Respond with True or False with best headers names in an array.\
+            Example: {"True": [Best Headers Response]}"""
+    prompt = f"{keyword}\n\n{header}\n\n"
+    result = openai_call(prompt, 0.1)
+    result = eval(result)
+    for k, v in result.items():
+        if k == "False":
+            df = pd.DataFrame(np.row_stack([df.columns, df.values]),
+                             columns=v)
+            return df
+    return df
+
 def convert_file(filename: str):
     """Converts csv/pdf/xls files to json
 
@@ -32,7 +58,11 @@ def convert_file(filename: str):
         object: json
     """
     if filename.endswith(".csv"):
-        df = pd.read_csv(filename)
+        with open(filename) as f:
+            encoding = f.encoding
+        df = pd.read_csv(filename, encoding=encoding)
+        header = df.head(2).to_csv()
+        df = check_header(df, header)
         response = df_to_json(df)
         return response
 
