065dccad6125aadd325385a3017a874ff58296e6
Joseun <ologanj@gmail.com>
2022-12-17 10:06:04 +0100

    feat(BAE 59): new json response based on frontend specifications

--- Backend/controllers/matching.py
+++ Backend/controllers/matching.py
@@ -1,50 +1,4 @@
 #!/usr/bin/python3
-# """ DOCUMENT MATCHING MODULE """
-# import asyncio
-# import pandas as pd
-
-# from .convert_file import convert_file
-# from .openai_request import openai_call
-
-
-# def match(file1, file2):
-#     """Matches similar transactions in two documents
-#     Args:
-#         file1: first document uploaded
-#         file2: second document uploaded
-#     Return:
-#         object: json
-#     """
-#     keyword = """
-#         Match all details in these files content below. No title.
-#         Response in JSON\n
-#         """
-#     statement_table = convert_file(file1)
-#     statement_table = pd.DataFrame(statement_table)
-#     records_table = convert_file(file2)
-#     records_table = pd.DataFrame(records_table)
-#     statement_csv = statement_table.to_csv()
-#     records_csv = records_table.to_csv()
-
-#     columns = list(statement_table.columns) + list(records_table.columns)
-
-#     example = "Example\n[\n{"
-
-#     for x in columns:
-#         example += f"\n    \"{x}\":"
-
-#     example += "\n  }\n]"
-
-#     prompt = f"{keyword}{example}{statement_csv}\n{records_csv}"
-
-#     response = openai_call(prompt)
-    # try:
-    #     matched_response = eval(response)
-    #     return matched_response
-    # except:
-    #     index = response.index('[')
-    #     matched_response = eval(response[index:])
-    #     return matched_response
 """ DOCUMENT MATCHING MODULE """
 import pandas as pd
 
@@ -52,38 +6,6 @@ from .convert_file import convert_file, df_to_json
 from .openai_request import openai_call
 from .get_sections import get_sections
 
-
-# def match(file1, file2):
-#     """Matches similar transactions in two documents
-#     Args:
-#         file1: first document uploaded
-#         file2: second document uploaded
-#     Return:
-#         object: json
-#     """
-#     keyword = """
-#         Match all details in these files content below. No title.
-#         Response in JSON\n
-#         """
-#     statement_table = pd.read_json(convert_file(file1))
-#     records_table = pd.read_json(convert_file(file2))
-    
-#     statement_csv = statement_table.to_csv()[:1000]
-#     records_csv = records_table.to_csv()[:1000]
-
-#     columns = list(statement_table.columns) + list(records_table.columns)
-
-#     example = "Example\n[\n{"
-
-#     for x in columns:
-#         example += f"\n    \"{x}\":"
-
-#     example += "\n  }\n]"
-
-#     prompt = f"{keyword}{example}{statement_csv}\n{records_csv}"
-
-#     response = openai_call(prompt)
-#     return response
 
 def compare(df1, df2):
     response = []
@@ -127,8 +49,9 @@ def match(file1, file2):
         object: json
     """
     keyword = """
-        Match all the details in these files content below. No title.
-        Response must be a JSON in an array. All key and values in double quotations"\n
+        Match all the details in these files content below. No title. \
+        Response must be a JSON in an array. Fill empty list with dictionary of empty string values.
+        All key and values in double quotations"\n
         """
     statement_table = convert_file(file1)
     statement_table = pd.DataFrame(statement_table)
@@ -154,7 +77,7 @@ def match(file1, file2):
 
     example += "\n   Matching: Yes\n   Matching_details:\n   [\n   {"
     for x in columns_b:
-        example += f"\n    \"{x}\":"
+        example += f"\n    \"{x}\": "
     example += "\n   }\n   ]\n}\n]"
 
     prompt = f"{keyword}{example}{statement_csv}\n{records_csv}"
