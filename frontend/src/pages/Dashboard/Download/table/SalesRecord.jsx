db56c75e2c214184baa7f6d9df896eaddfa9ef38
jephpico <amadasun.jephthah@gmail.com>
2023-01-01 18:36:37 +0100

    date format-- bug fix

--- frontend/src/pages/Dashboard/Download/table/SalesRecord.jsx
+++ frontend/src/pages/Dashboard/Download/table/SalesRecord.jsx
@@ -11,25 +11,13 @@ import {
 	Flex,
 	Box,
 } from '@chakra-ui/react';
-import { BsSearch } from 'react-icons/bs';
-import {
-	TableDropDown,
-	// TableDropDownIcon,
-	TableSearchBox,
-} from '../minicomponents/MiniTableComponents';
+import Select from 'react-select';
+import { useAuth } from '../../../../Store/Context';
 
-// eslint-disable-next-line react/prop-types
-const SalesRecord = ({ tableRight, rightHeaderKeys }) => {
-	// eslint-disable-next-line react/prop-types
-	const headerKeys = rightHeaderKeys.slice(0, 4).map((key, index) => (
-		// eslint-disable-next-line react/no-array-index-key
-		<Th key={index} color="#fff">
-			<Box p={3}>{key}</Box>
-		</Th>
-	));
+const TableData = ({ tableRight }) => {
+	const [tableItems, setTableItems] = React.useState([...tableRight]);
 
-	// eslint-disable-next-line react/prop-types
-	const data = tableRight?.map((value, index) => {
+	return tableItems?.map((value, index) => {
 		const keys = Object.values(value);
 
 		if (keys[0] !== '') {
@@ -42,7 +30,6 @@ const SalesRecord = ({ tableRight, rightHeaderKeys }) => {
 					borderY="8px solid #fff"
 					bg="#B7EDD6"
 				>
-					{/* <Td>{value.Date}</Td> */}
 					<Td>
 						<Box p={3}>{keys[0]}</Box>
 					</Td>
@@ -58,7 +45,32 @@ const SalesRecord = ({ tableRight, rightHeaderKeys }) => {
 				</Tr>
 			);
 		}
-		// return <Td>{keys[0]}</Td>;
+		const { localData3 } = useAuth();
+		const options = [];
+		localData3[1].forEach((item) => {
+			const label = Object.values(item).join(' | ');
+			const valueio = item;
+
+			options.push({
+				label,
+				value: valueio,
+			});
+		});
+
+		const styles = {
+			option: (provided, state) => ({
+				...provided,
+				fontWeight: state.isSelected ? 'bold' : 'normal',
+				color: 'black',
+				backgroundColor: 'white',
+				width: '25em',
+			}),
+			singleValue: (provided, state) => ({
+				...provided,
+				color: 'black',
+				width: '25em',
+			}),
+		};
 
 		return (
 			<Tr key={uuidv4()} height="65px" borderBottom="8px solid #fff">
@@ -70,17 +82,44 @@ const SalesRecord = ({ tableRight, rightHeaderKeys }) => {
 						gap={2}
 						justifyContent="space-between"
 					>
-						<Box flex={1}>
-							<TableSearchBox />
-						</Box>
-						<Box>
-							<TableDropDown />
+						<Box className="w-full">
+							<Select
+								defaultValue={options[0]}
+								options={options}
+								styles={styles}
+								isSearchable
+								isClearable
+								onChange={(selectedOption) => {
+									const newTable = [...tableItems];
+									newTable.splice(index, 1, selectedOption.value);
+
+									options.forEach((item, i) => {
+										if (item.label === selectedOption.label) {
+											delete item[[i]];
+										}
+									});
+									setTableItems(newTable);
+								}}
+							/>
 						</Box>
 					</Flex>
 				</Td>
 			</Tr>
 		);
 	});
+};
+
+// eslint-disable-next-line react/prop-types
+const SalesRecord = ({ tableRight, rightHeaderKeys }) => {
+	// eslint-disable-next-line react/prop-types
+	const headerKeys = rightHeaderKeys.slice(0, 4).map((key, index) => (
+		// eslint-disable-next-line react/no-array-index-key
+		<Th key={index} color="#fff">
+			<Box p={3}>{key}</Box>
+		</Th>
+	));
+
+	// eslint-disable-next-line react/prop-types
 
 	return (
 		<TableContainer>
@@ -88,7 +127,9 @@ const SalesRecord = ({ tableRight, rightHeaderKeys }) => {
 				<Thead height="65px">
 					<Tr bg="#000">{headerKeys}</Tr>
 				</Thead>
-				<Tbody>{data}</Tbody>
+				<Tbody>
+					<TableData tableRight={tableRight} />
+				</Tbody>
 			</Table>
 		</TableContainer>
 	);
