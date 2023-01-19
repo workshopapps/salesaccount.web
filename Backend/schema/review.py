9d0e494fb407fe02e7cc13e3f770b5b6d24133bc
lone-wolve <ai21821021@utg.edu.gm>
2023-01-19 10:26:06 +0000

    add pydantic model for entry validation

---
+++ Backend/schema/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Review module """
+from pydantic import BaseModel
+
+
+class Review(BaseModel):
+    id: str
+    email: str
+    text: str 
+    stars: int
+    date: float 
