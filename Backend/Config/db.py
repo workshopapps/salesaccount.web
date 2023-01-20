ed735c66b141a70dddba7e3823a122c24e15731c
Chidubem <nwabuisicelestine@gmail.com>
2023-01-20 22:12:38 +0100

    modified db config and updated requirements.txt

--- Backend/Config/db.py
+++ Backend/Config/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 """Database connection to xamp server """
-DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/Reconcile"
+DATABASE_URL = "mysql+mysqlconnector://reconcileai:HNG#9reconcileai@localhost:3306/Reconcile"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit= False, autoflush=False, bind=engine)
 Base = declarative_base()
